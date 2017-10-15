bf6e6920254408df880a389340388b9e93be85eb
nikitazozoulenko <nikita.zozoulenko@gmail.com>
2017-10-15 21:57:20 +0200

    finished planning the model, time to complete this project :snake: :snake

--- yolo_net.py
+++ yolo_net.py
@@ -53,7 +53,12 @@ def YOLO_network(x, is_training, confidence_threshhold = 0.5):
         x = x = conv_wrapper(x, shape = [1,1,256,B*5+C], strides = [1, 1, 1, 1], padding = "VALID")
     return x
 
-def loss(yolo_output): #size is None x 7 x 7 x 30, P, X, Y, WIDTH, HEIGHT, P, X, Y, WIDTH, HEIGHT, 20C
+def image_detection_op(yolo_tensor, image_size):
+    #input: 1 yolo_tensor size None x 7 x 7 x 30:      P, X, Y, WIDTH, HEIGHT, P, X, Y, WIDTH, HEIGHT, 20C
+
+    #output: 0-98 bndbox + class prediction
+
+def train_op(yolo_tensor): #size is None x 7 x 7 x 30, P, X, Y, WIDTH, HEIGHT, P, X, Y, WIDTH, HEIGHT, 20C
     #1st: match gt-label with appropriate grid cell
     S = 7
     gt = (174/486, 101/500, 349/486, 351/500) #xmin ymin xmax ymax
@@ -62,9 +67,9 @@ def loss(yolo_output): #size is None x 7 x 7 x 30, P, X, Y, WIDTH, HEIGHT, P, X,
     cell_y = int((ymin + ymax)/2 * S)
     cell_x = int((xmin + xmax)/2 * S)
 
-    bndbox1 = yolo_output[0, cell_y, cell_x, 1:5] # [x, y, width, height]
+    bndbox1 = yolo_tensor[0, cell_y, cell_x, 1:5] # [x, y, width, height]
     bndbox1_confidence = yolo_output[0, cell_y, cell_x, 0]
-    bndbox2 = yolo_output[0, cell_y, cell_x, 6:10]
+    bndbox2 = yolo_tensor[0, cell_y, cell_x, 6:10]
     bndbox2_confidence = yolo_output[0, cell_y, cell_x, 5]
 
     bndbox1_iou = iou(box1 = (bndbox1[0], bndbox1[1], bndbox1[2]+bndbox1[0], bndbox1[3]+bndbox1[1]),
@@ -72,7 +77,6 @@ def loss(yolo_output): #size is None x 7 x 7 x 30, P, X, Y, WIDTH, HEIGHT, P, X,
 
     bndbox2_iou = iou(box1 = (bndbox2[0], bndbox2[1], bndbox2[2]+bndbox2[0], bndbox2[3]+bndbox2[1]),
                       box2 = gt)
-
 
     if(bndbox1_iou > bndbox2_iou):
         box_index = 0
@@ -84,6 +88,11 @@ def loss(yolo_output): #size is None x 7 x 7 x 30, P, X, Y, WIDTH, HEIGHT, P, X,
     alpha_obj = 1
 
 def iou(box1, box2):
+    #input
+    #box1: (xmin1, ymin1, xmax1, ymax1)
+    #box2: (xmin2, ymin2, xmax2, ymax2)
+
+    #output: float
     xmin, ymin, xmax, ymax = box1
 
     predxmin, predymin, predxmax, predymax = box2
@@ -93,9 +102,9 @@ def iou(box1, box2):
     y0 = max(ymin, predymin)
     y1 = min(ymax, predymax)
 
-    intersection_area = (x1-x0+1) * (y1-y0+1)
-    pred_area = (predxmax - predxmin + 1) * (predymax - predymin + 1)
-    gt_area = (xmax - xmin + 1) * (ymax - ymin + 1)
+    intersection_area = (x1-x0) * (y1-y0)
+    pred_area = (predxmax - predxmin) * (predymax - predymin)
+    gt_area = (xmax - xmin) * (ymax - ymin)
     iou = intersection_area / (gt_area + pred_area - intersection_area)
 
     return iou
